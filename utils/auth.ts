1d5ff4f89a5678e52abafcfe98ee56903bc2e5e0
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-03-15 18:40:21 +0000

    added auth.ts file

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,1 +1,28 @@
-// TODO: Implement ./utils/auth.ts
+// auth.ts
+import { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./lib/db";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async session({ session, user }) {
+      // Add user ID to the session
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
